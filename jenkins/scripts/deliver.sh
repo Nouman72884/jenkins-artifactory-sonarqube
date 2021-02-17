57f80949f9a0e213c821a31567b892b2d45e1411
Nouman72884 <noumansatti7@gmail.com>
2021-02-17 07:42:14 +0500

    initial commit

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,7 +5,7 @@ sudo apt-get install default-jre -y
 sudo apt-get install apache2 -y
 echo "entering html"
 chmod 777 /var/www/html
-sudo cp my-app-1.0-SNAPSHOT.110.jar /var/www/html
+sudo cp my-app-1.0-SNAPSHOT.jar /var/www/html
 ls
 echo "deploying app"
 cd  /var/www/html
@@ -13,4 +13,4 @@ ls
 sudo rm index.html
 echo "ls inside"
 ls
-nohup java -jar my-app-1.0-SNAPSHOT.110.jar > index.html 2>&1 &
+nohup java -jar my-app-1.0-SNAPSHOT.jar > index.html 2>&1 &
